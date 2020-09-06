6e2ebbaae3f8928ac17ac5b254c6a32d60aa63f6
ukrgerri4 <ukrgerri4@gmail.com>
2020-09-07 01:01:27 +0300

    added jwt generation

--- BoardWebApp/Controllers/AuthController.cs
+++ BoardWebApp/Controllers/AuthController.cs
@@ -2,7 +2,13 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BoardApp.Controllers
@@ -13,21 +19,53 @@ namespace BoardApp.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
 
         public AuthController(
             UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager
+            SignInManager<IdentityUser> signInManager,
+            IConfiguration configuration
         )
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _configuration = configuration;
         }
 
         [AllowAnonymous]
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody]LoginQM model)
         {
-            return Ok(await _userManager.FindByNameAsync(model.UserName));
+            var accessToken = GenerateToken(await _userManager.FindByNameAsync(model.UserName));
+            return Ok(new { accessToken });
+        }
+
+        private string GenerateToken(IdentityUser user)
+        {
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Authorization:Jwt:SecretKey"]));
+
+            var tokenIssueDateTime = DateTime.UtcNow;
+            var tokenExpireDateTime = tokenIssueDateTime.Add(TimeSpan.FromMinutes(10));
+            var claims = new List<Claim>()
+                {
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Sid, user.Id)
+                };
+
+            var jwtToken = new JwtSecurityToken(
+                //issuer: _configuration["Authorization:Jwt:Issuer"],
+                //audience: _configuration["Authorization:Jwt:Audience"],
+                claims: claims.ToArray(),
+                //notBefore: tokenIssueDateTime,
+                expires: tokenExpireDateTime,
+                signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
+            );
+
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+
+            var encodedJwt = jwtSecurityTokenHandler.WriteToken(jwtToken);
+
+            return encodedJwt;
         }
     }
 }
